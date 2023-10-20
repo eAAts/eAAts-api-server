c894c89b7f2d66e2551f2602a26059d154af5b80
천정현 <ouoabcde@naver.com>
2023-10-20 15:19:36 +0900

    Add eAAts event entities

--- constants/eAAts_query_entities.js
+++ constants/eAAts_query_entities.js
@@ -1,5 +1,11 @@
 const orderCreateds = "id\norderId\ncreator\nminParticipants\nfeeType\nblockNumber\nblockTimestamp\ntransactionHash";
+const orderJoineds = "id\norderId\nblockNumber\nblockTimestamp\ntransactionHash";
+const orderDeliveryStarteds = "id\norderId\nblockNumber\nblockTimestamp\ntransactionHash";
+const deliveryCompleteds = "id\norderId\nblockNumber\nblockNumber\blockTimestamp\ntransactionHash";
 
 module.exports = {
     orderCreateds,
+    orderJoineds,
+    orderDeliveryStarteds,
+    deliveryCompleteds,
 };
